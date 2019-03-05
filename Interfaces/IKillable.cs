03ffac03428bcea73fa22f17c07de9130862d6f1
Wario <37343942+IfreannMedia@users.noreply.github.com>
2019-03-05 21:20:46 +0000

    Add files via upload

---
+++ Interfaces/IKillable.cs
@@ -0,0 +1,7 @@
+﻿
+// Interface for getting killed, implemented by the enemies and the player
+// in PlayerLifeCycle.cs and EnemyLifeCycle.cs
+public interface IKillable {
+
+   void KillEntity();
+}
