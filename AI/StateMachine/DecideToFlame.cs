065617a5ffd75ad3c4a65481c2769ffd0fc7b10f
Wario <37343942+IfreannMedia@users.noreply.github.com>
2019-03-05 21:13:16 +0000

    Add files via upload

---
+++ AI/StateMachine/DecideToFlame.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(menuName = "PluggableAI/Enemy Decisions/Decide To Flame")]
+public class DecideToFlame : Decision
+{
+
+    public override bool Decide(StateController controller)
+    {
+        if (controller.InFlamethrowerRange
+            && controller.EnemySightScript.PlayerInSight)
+        {
+            controller.AnimatorController.SetBool(controller.TagsHash.InFlamethrowerRange, controller.InFlamethrowerRange);
+            return true;
+        }
+        else
+        {
+            controller.AnimatorController.SetBool(controller.TagsHash.InFlamethrowerRange, controller.InFlamethrowerRange);
+            return false;
+        }
+    }
+}
