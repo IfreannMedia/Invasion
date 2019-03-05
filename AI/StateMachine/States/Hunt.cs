4fa6b560b89cc9e336af588647238db103cb5f57
Wario <37343942+IfreannMedia@users.noreply.github.com>
2019-03-05 21:15:39 +0000

    Add files via upload

---
+++ AI/StateMachine/States/Hunt.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//----------------------------------------------------------------------------------
+// CLASS:       Hunt.cs, Inherits from State.cs which is a ScriptableObject Class
+// DESCRIPTION: This is the main state enemies use for getting into attacking range
+//              of the player, from which they then transition into an attack state
+//-----------------------------------------------------------------------------
+[CreateAssetMenu(menuName = "PluggableAI/EnemyState/Hunt Player")]
+public class Hunt : State
+{
+
+
+}
---
+++ AI/StateMachine/States/MeleeAttackState.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(menuName = "PluggableAI/EnemyState/Basic Melee Attack State")]
+public class MeleeAttackState : State
+{
+
+
+}
