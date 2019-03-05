91a32cc14f51441061df556951d7639ee74878d8
Wario <37343942+IfreannMedia@users.noreply.github.com>
2019-03-05 21:16:08 +0000

    Add files via upload

---
+++ ChooseTargetAnimation.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+// -----------------------------------------------------------------------------------
+// CLASS:       ChooseTargetAnimation
+// DESCRIPTION: Asign this in an inspector to pass an integer value to the objects animator
+//              this will asign an animation state to that object
+//              Each red target in the start scene has it's own aninmation state, 
+//              but they all share the same controller
+// -----------------------------------------------------------------------------------
+public class ChooseTargetAnimation : MonoBehaviour
+{
+
+    public int TargetAnim = 0;
+
+    private void Awake()
+    {
+        GetComponent<Animator>().SetInteger("Anim", TargetAnim);
+    }
+}
