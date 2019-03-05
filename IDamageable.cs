792f61956e9410610b10e970e180388c69fc6b26
Wario <37343942+IfreannMedia@users.noreply.github.com>
2019-03-05 21:20:10 +0000

    Add files via upload

---
+++ IDamageable.cs
@@ -0,0 +1,7 @@
+﻿
+// Interface for taking damage, implemented by the enemies and the player
+// in PlayerLifeCycle.cs and EnemyLifeCycle.cs
+public interface IDamageable {
+
+    void TakeDamage(float damage);
+}
