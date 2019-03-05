8d3d8a533337b46015021b6690f8f9ae7536399b
Wario <37343942+IfreannMedia@users.noreply.github.com>
2019-03-05 21:12:03 +0000

    Add files via upload

---
+++ AI/StateMachine/Action.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// -------------------------------------------------------------
+// CLASS: Action
+// DESC:  Abstract base class for all other actions
+// -------------------------------------------------------------
+public abstract class Action : ScriptableObject
+{
+    // Abstract method to be overriden, takes a stateController parameter, will be executed in update loop
+    public abstract void Act(StateController controller);
+
+}
