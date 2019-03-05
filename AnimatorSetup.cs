c0a3165e12c4ce18c8524cf00cb14c864a49ade5
Wario <37343942+IfreannMedia@users.noreply.github.com>
2019-03-05 21:01:48 +0000

    Add files via upload

---
+++ AnimatorSetup.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+// -----------------------------------------------------------------------------------------------------------------------------------------
+// CLASS: AnimatorSetup
+// Desc:  Helps to set up enemy animation by passing vertical and angular velocity parametersto the animator controller.
+//        It also applies damping to avoid erratic movement when the agent is turning.
+//        The code is adopted from the unity stealth game tutorial, which is no longer on their website, but can be found
+//        on their youtube channel. I chose to implemente it after a few failed iterations at properly coupling enemy navigation and animation
+// --------------------------------------------------------------------------------------------------------------------------------------------
+public class AnimatorSetup
+{
+    // Floats to control the damping of angular speed and speed, VelX and VelY params in anim controller respectively
+    public float SpeedDampTime = 0.1f, AngularSpeedDampTime = 0.7f;
+    public float AngleResponseTime = 0.6f; // Used to convert the turning angle into angular speed param
+
+    private Animator _anim;
+    private TagsHashIDs _hashIDs;
+
+    // Constructor, called by other script when an instance of this class is created:
+    public AnimatorSetup(Animator anim, TagsHashIDs tagsHashes)
+    {
+        _anim = anim;
+        _hashIDs = tagsHashes;
+    }
+
+    // -----------------------------------------------------------------------------------------------------
+    // METHOD:      Setup()
+    // Desciption:  Called from EnemyAnimation.cs file in NavAnimSetup() method, which is called every 
+    //              frame update. The speed parameter is the magnitude of the forward facing vector of the agent
+    //              projected onto the local desired velocity vector. The angle is simply the angle between
+    //              these two vectors. We use this method to pass these values into the animator controller,
+    //              dividing the angle by the angle response time, in order to achieve a smother rotation
+    // ------------------------------------------------------------------------------------------------------
+    public void Setup(float speed, float angle)
+    {
+        float angularSpeed = angle / AngleResponseTime;
+        // Pass values to animator controller VelX and VelY
+        _anim.SetFloat(_hashIDs.VelY, speed, SpeedDampTime, Time.deltaTime);
+        _anim.SetFloat(_hashIDs.VelX, angularSpeed, AngularSpeedDampTime, Time.deltaTime);
+    }
+}
---
+++ DoorOpener.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// CLASS: DoorOpener
+// Desc:  This script lerps the vector3 position of a gameobject in a certain direction,
+//        And for the third lerp parameter passes in a point on an animation curve
+//        In order to prevent erroneous method calls the script instance destroys itself once the ienumerator is called
+
+public class DoorOpener : MonoBehaviour {
+
+
+    public AnimationCurve ObjectCurve = new AnimationCurve();
+    public ParticleSystem[] _steamParticles = new ParticleSystem[2];
+    [SerializeField] float _distance = 4f;
+    [SerializeField] float _duration = 0.5f;
+
+    private Transform _transform;
+    private Vector3 _startPos, _endPos;
+    private AudioSource _doorAudio;
+
+    void Start () {
+        _transform = GetComponentInChildren<Transform>();
+        _startPos = _transform.position;
+        _endPos = _startPos + (-_transform.up * _distance);
+        _doorAudio = GetComponent<AudioSource>();
+    }
+	
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(TagsHashIDs.Player))
+        {
+            _doorAudio.PlayOneShot(_doorAudio.clip);
+            foreach (ParticleSystem ps in _steamParticles)
+            {
+                ps.Play();
+            }
+            StartCoroutine(MoveDoor());
+        }
+    }
+
+    IEnumerator MoveDoor()
+    {
+       // _currentState = ObjectState.Animating;
+        float timer = 0.0f;
+
+        // is our goal the start state? then our start position is the end position
+        //Vector3 startPos = (goalState == ObjectState.Start) ? _endPos : _startPos;
+        //Vector3 endPos = (goalState == ObjectState.Start) ? _startPos : _endPos;
+
+        while (timer <= _duration)
+        {
+            // Lerp to the end position, each lerp dictated by the playhead position on the curve, dictated by normalized float t
+            float t = timer / _duration;
+            _transform.position = Vector3.Lerp(_startPos, _endPos, ObjectCurve.Evaluate(t));
+
+            timer += Time.deltaTime;
+            yield return null;
+        }
+        // Snap the object ot it's end pos just in case, and set the current object state to the passed in value
+        _transform.position = _endPos;
+        Destroy(this);
+    }
+}
