5e0a0d57077a82050b77cffd21a856543c40528f
Wario <37343942+IfreannMedia@users.noreply.github.com>
2019-03-05 21:14:53 +0000

    Add files via upload

---
+++ AI/StateMachine/ArmouredEnemyShootState.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(menuName = "PluggableAI/Enemy States/Armoured Enemy Shoot State")]
+public class ArmouredEnemyShootState : State
+{
+
+}
