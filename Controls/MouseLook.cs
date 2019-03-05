9887027221b3c745442ef34d13f733ff6c864c99
Wario <37343942+IfreannMedia@users.noreply.github.com>
2019-03-05 21:18:23 +0000

    Add files via upload

---
+++ Controls/MouseLook.cs
@@ -0,0 +1,116 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+// ----------------------------------------------------------------------------------------------------------------
+// CLASS:       MouseLook..cs
+// DESCRIPTION: This cass gets user input from mouse and joystick controller (only tested with PS4 dualshock models)
+//              And tranlsates that input to the camera rotation on it's X-axis and the Player's character controller 
+//              rotation on it's Y-axis
+//------------------------------------------------------------------------------------------------------------------
+public class MouseLook : MonoBehaviour {
+
+    // Public Fields
+    [Header("Look Settings")]
+    public float lookSensitivity = 3f;
+    public float clampValue = 80.0f;
+    public bool inverted = false;
+    public Slider AimSlider;
+
+    // Private Fields
+    private CharacterController playerCharController;
+    private float _rotationX = 0.0f; // the rotation of the camera on the X-axis
+    private MoveScript _moveScript; // Used to detect if we are currently sprinting
+    private string _mouseX = "Mouse X", _joyX = "Joy X"; // Cached names for X axis input, depending on whether using mouse or controller
+    private string _mouseY = "Mouse Y", _joyY = "Joy Y"; // cahced names for Y axis input
+
+    // ----------------------------------------------------
+    // Method:      Awake()
+    // Description: Lock the cursor, cache global variables
+    //-----------------------------------------------------
+    void Awake()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        playerCharController = GetComponentInParent<CharacterController>();
+        _moveScript = GetComponentInParent<MoveScript>();
+    }
+	
+    // ----------------------------------------------------------------------------------------------------------------
+    // Method:      Update()
+    // Description: This method gets input from both the mouse and the anolog sticks of a joypad
+    //              Joypad input is pre-configured to work with a dualshock PS4 controller - it has not been tested with 
+    //              xBox controller or other varients
+    //------------------------------------------------------------------------------------------------------------------
+	void Update () {
+        // We cannot control the player's cam rotation if the game is paused:
+        if (Time.timeScale < 1.0f)
+        {
+            return;
+        }
+
+        // rotate the player along it's Y access to turn left and right
+        // If we are sprinting, we cannot rotate as fast:
+        if (_moveScript.isSprinting)
+        {
+            playerCharController.transform.Rotate(0.0f, Input.GetAxis(_mouseX) * (lookSensitivity * 0.50f), 0.0f);
+
+            if (Input.GetAxis(_joyX) != 0)
+            {
+                playerCharController.transform.Rotate(0.0f, Input.GetAxis(_joyX) * (lookSensitivity * 0.50f), 0.0f);
+            }
+        }
+        else
+        {
+            playerCharController.transform.Rotate(0.0f, Input.GetAxis(_mouseX) * lookSensitivity, 0.0f);
+            if (Input.GetAxis(_joyX) != 0)
+            {
+                playerCharController.transform.Rotate(0.0f, Input.GetAxis(_joyX) * lookSensitivity, 0.0f);
+            }
+
+        }
+
+        // Rotate the camera object along it's x access, depending on inversion:
+        if (inverted)
+        {
+            _rotationX += Input.GetAxis(_mouseY) * lookSensitivity;
+            _rotationX += Input.GetAxis(_joyY) * lookSensitivity; 
+        }
+        else
+        {
+            _rotationX -= Input.GetAxis(_mouseY) * lookSensitivity;
+            _rotationX -= Input.GetAxis(_joyY) * lookSensitivity;
+        }
+         
+
+
+        _rotationX = Mathf.Clamp(_rotationX, -clampValue, clampValue); // clamp the vertical look rotation
+
+        float rotationY = transform.localEulerAngles.y; // grab the current y rotation which we have already set
+
+        transform.localEulerAngles = new Vector3(_rotationX, rotationY, 0.0f); // assign the camera's rotation
+
+    }
+
+    // ----------------------------------------------------------------------------------------------------------------
+    // Method:      UpdateLookSensitivity()
+    // Description: This method is called from the Pause Game canvas Aim Slider when it's value is changed
+    //              The slider value is clamped to be between 1 and 6, it is how users adjust aim sensitivity for both
+    //              horizontal and vertical look axis
+    //------------------------------------------------------------------------------------------------------------------
+    public void UpdateLookSensitivity()
+    {
+        lookSensitivity = AimSlider.value;
+
+    }
+
+    //---------------------------------------------------------------------------
+    // Method:      InvertCamera()
+    // Description: Inverts the camera's vertical input using a ternery operator
+    //              Called from pause game canvas button
+    //---------------------------------------------------------------------------
+    public void InvertCamera()
+    {
+        inverted = inverted == true ? false : true;
+    }
+}
+
---
+++ Controls/MoveScript.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using UnityEngine;
+//---------------------------------------------------------------------------------------------------
+// CLASS:       MoveScript.cs
+// DESCRIPTION: Responsible for moving the player's capsule around the level geometry using WASD keys
+//              Uses the unity CharacterController component, which is useful for going up steps and
+//              Eliminating wall-sliding
+//---------------------------------------------------------------------------------------------------
+
+public class MoveScript : MonoBehaviour {
+
+    // Public fields:
+    [HideInInspector] public bool grounded;
+
+    private CharacterController playerController;
+    private Animator animator;
+    // Visible in Inspector
+    [Header("Controller Settings")]
+    [SerializeField] private float jumpVelocity;
+    [SerializeField] private float speed;
+    [Range(0.0f,5.0f)] private float sprintMeter = 5.0f; // Max amount of possible sprint, equivelint to time ie 5.0 seconds
+    [HideInInspector] public bool isSprinting = false;
+    [SerializeField] private float gravity = -9.7f;
+    
+    private Animator playerAnim;
+    private PlayerLifecycle _lifeCycleScript;
+
+
+    //-------------------------------
+    // Method:      Start()
+    // Description: Cache components
+    //-------------------------------
+    void Start()
+    {
+        playerController = GetComponent<CharacterController>();
+        playerAnim = GetComponent<Animator>();
+    }
+
+    //----------------------------------------------------------------------------------------------------------------------------------
+    // Method:      Update()
+    // Description: listens for user input. Allows user to move the character by implementing the CharacterController.Move()
+    //              method. This avoid slippery sliding on the floor and avoids the character getting snagged or stuck on env objects
+    //              which was a common issue with using a rigidbody controller
+    //----------------------------------------------------------------------------------------------------------------------------------
+    private void Update()
+    {
+
+        if (Input.GetButtonDown("Sprint") && sprintMeter > 0.0f) // Left Shift is sprint
+        {
+            speed = 9;
+            isSprinting = true;
+            StartCoroutine(Sprint());
+        }
+        else if (Input.GetButtonUp("Sprint") || sprintMeter <= 0.0f) // if we release sprint or run out of sprint meter disable it
+        {
+            speed = 6.35f;       // 6.45f is standard speed
+            isSprinting = false; // the MouseLook.cs script likes to read this boolean value
+        }
+
+        Vector3 playerMovement = new Vector3(Input.GetAxis("Horizontal") * speed, 0.0f, Input.GetAxis("Vertical") * speed);
+        playerMovement = Vector3.ClampMagnitude(playerMovement, speed); // Use clamp magnitute to make sure the speed has a hard maximum limit
+        playerMovement.y = gravity;
+        playerMovement *= Time.deltaTime;
+        playerMovement = transform.TransformDirection(playerMovement);
+        playerController.Move(playerMovement);
+
+        if (playerController.isGrounded)
+        {
+            grounded = true;
+            playerAnim.SetFloat("VelocityX", playerController.velocity.magnitude); // this param is used for the 1D locomotion blend tree
+        }
+        else
+        {
+            grounded = false;
+        }
+
+    }
+    // ---------------------------------------------------------------------------------
+    // Method: Sprint()
+    // Desc:   Depletes the sprintmeter by 1 unit every second. Increases sprint meter
+    //         by a half unit every second the player is not sprinting
+    //         I chose not to display a sprint timer as few modern FPS games do this
+    // ---------------------------------------------------------------------------------
+    IEnumerator Sprint()
+    {
+        while (isSprinting)
+        {
+            sprintMeter -= 1.0f;
+            yield return new WaitForSeconds(1.0f);
+        }
+        while (!isSprinting && sprintMeter < 5.0f)
+        {
+            sprintMeter += 0.25f;
+            yield return new WaitForSeconds(1.0f);
+        }
+        yield return null;
+    }
+}
+
